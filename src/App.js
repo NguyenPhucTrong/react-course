af630dc59888ec94309ec313409efbb15dc61159
NguyenPhucTrong <tronga7z6@gmail.com>
2024-02-22 22:57:50 +0700

    #09 Adding Child Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
+import "./app.css";
+import Products from "./Components/Products";
+
 function App() {
   // const p = document.createElement("p");
   // p.textContent = "This is My App";
   // return p;
-  let a1 = "abcdef";
-  return (
-    <div>
-      <p>
-        This is {a1} My App {10 + 5}
-      </p>
-    </div>
-  );
+  // let a1 = "abcdef";
+  return <Products />;
 }
 
 export default App;
