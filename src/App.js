520b0fb8c9b084c781b98e9b233a07e67be4fee3
NguyenPhucTrong <tronga7z6@gmail.com>
2024-02-22 22:07:10 +0700

    07 Introduction to JavaScript XML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // const p = document.createElement("p");
+  // p.textContent = "This is My App";
+  // return p;
+  let a1 = "abcdef";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        This is {a1} My App {10 + 5}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const header = <h1> This is my first React App</h1>;
+// const header2 = <h1> This is my first React App2</h1>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // console.log(header);
+
+// ReactDOM.render(header, document.getElementById("root"));
+// ReactDOM.render(header2, document.getElementById("text"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+// let div = document.getElementById("root");
+// div.append(App());
